2ff9ebe0103f33139991c4faeb980f2ce7a28548
Solemark <larco9mason@gmail.com>
2024-11-02 11:29:43 +1000

    expand test coverage

--- test/swap_variables_test.py
+++ test/swap_variables_test.py
@@ -1,5 +1,19 @@
+from typing import Any
+from pytest import fixture
+
 from src.swap_variables import swap
 
 
-def test_swap_variables() -> None:
-    assert 2, 1 == swap(1, 2)
+@fixture
+def data() -> list[list[tuple[Any, Any]]]:
+    return [
+        [(2, 1), (1, 2)],
+        [("abcd", "1234"), ("1234", "abcd")],
+        [(True, False), (False, True)],
+    ]
+
+
+def test_swap_variables(data: list[list[tuple[Any, Any]]]) -> None:
+    for row in data:
+        exp, inp = row
+        assert exp == swap(inp[0], inp[1])
